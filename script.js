66fc7780c04a2b9a08fefcc11a0d740657b8ab6b
Strongic <lstrong8@msudenver.edu>
2026-04-21 14:50:47 -0600

    returning number of cut pieces to html

--- script.js
+++ script.js
@@ -1,6 +1,95 @@
 const recipes = {
-    "DW2 Wall": { "OSB Sheets": 2, "2x4 Studs": 4 },
+    "DW2 Wall": {
+        "4x8' OSB": {
+            totalBoardsNeeded: 1,
+            yieldPerBoard: 1,
+            cutPiecesNeeded: 1,
+            cutLength: "84\""
+
+        },
+        "2x4x8' Board": {
+            totalBoardsNeeded: 4,
+            yieldPerBoard: 1,
+            cutPiecesNeeded: 4,
+            cutLength: "79\"" //double check length
+        }
+
+    },
+
+    "DW2 Pallet": {
+        "2x8x8' Board": {
+            totalBoardsNeeded: 2,
+            yieldPerBoard: 1,
+            cutPiecesNeeded: 2,
+            cutLength: "84\""
+        },
+
+        "2x6x8' Board": {
+            totalBoardsNeeded: 2, //overlap since we will have remainder of 2
+            yieldPerBoard: 3,
+            cutPiecesNeeded: 4,
+            cutLength: "30\""
+        },
+        "4x6x8' Board": {
+            totalBoardsNeeded: 1, //overlap b/c we will have remainder
+            yieldPerBoard: 12,
+            cutPiecesNeeded: 8,
+            cutLength: "8\""
+        },
+        "1x6x8' Board": {
+            totalBoardsNeeded: 2,
+            yieldPerBoard: 1,
+            cutPiecesNeeded: 2,
+            cutLength: "84\""
+        },
+    },
+
+    "Mini Wall": {
+        "4x8' OSB": {
+            totalBoardsNeeded: 1,
+            yieldPerBoard: 1,
+            cutPiecesNeeded: 1,
+            cutLength: "74\""
+        },
+
+        "2x4x8' Board": {
+            totalBoardsNeeded: 3,
+            yieldPerBoard: 1,
+            cutPiecesNeeded: 3,
+            cutLength: "69\"" //double check length
+        },
+
+    },
+
+    "Mini Pallet": {
+        "2x8x8' Board": {
+            totalBoardsNeeded: 2,
+            yieldPerBoard: 1,
+            cutPiecesNeeded: 2,
+            cutLength: "74\""
+        },
+
+        "2x6x8' Board": {
+            totalBoardsNeeded: 1,
+            yieldPerBoard: 3,
+            cutPiecesNeeded: 3,
+            cutLength: "30\""
+        },
+        "4x6x8' Board": {
+            totalBoardsNeeded: 1, //overlap b/c we will have remainder
+            yieldPerBoard: 12,
+            cutPiecesNeeded: 6,
+            cutLength: "8\""
+        },
+        "1x6x8' Board": {
+            totalBoardsNeeded: 2,
+            yieldPerBoard: 1,
+            cutPiecesNeeded: 2,
+            cutLength: "74\""
+        },
+    },
 }
+
 
 let savedChoice = ""; //global variable for storing what option the user clicked
 
@@ -15,13 +104,43 @@ function selectOption(name) {
     console.log("System ready for: " + savedChoice);
 }
 
+
+
 function performCalculation() {
-    const userNumber = document.getElementById('quantity').value; //grab the number of quantities
+    const qty = document.getElementById('quantity').value; //grab the number of quantities
+    const outputDiv = document.getElementById('result');
+    const ingredients = recipes[savedChoice] // get specific recipe 
 
-    const ingredients = recipes[savedChoice] // get specific recipe from the table
+    // if (!ingredients) {
+    //     console.log("0 was inputted");
+    //     return;
+    // }
 
-    for (const item in ingredients) {
-        let totalNeeded = ingredients[item] * userNumber;
-        console.log("You need " + totalNeeded + " of " + item);
+    console.log("Ingredients found for " + savedChoice + ":", ingredients);
+
+    //build header
+    let resultHTML = `<h2>Production Order: ${qty}x ${savedChoice}(s)</h2>`;
+    resultHTML += `<p><em>Based on infinite stock (ignoring board remainders)</em></p>`;
+    resultHTML += `<hr><h3>Total Cut List</h3><ul>`;
+
+
+
+    for (const materialName in ingredients) {
+        const data = ingredients[materialName];
+
+        //pieces per unit * total units
+        const totalPieces = data.cutPiecesNeeded * qty;
+
+        resultHTML += `
+            <li style="margin-bottom: 10px;">
+                <strong>${totalPieces} pieces</strong> of ${materialName} 
+                <br>Cut to length: <strong>${data.cutLength}</strong>
+            </li>`;
     }
+
+
+    resultHTML += "</ul>";
+
+    //push to html
+    outputDiv.innerHTML = resultHTML;
 }
